10f3ada0b5659ebeeef3f99158a952c7f8a5bfc1
Vinay Rajaratnam <vjrajara@calpoly.edu>
2021-01-13 16:20:14 -0800

    Linked frontend to backend

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import Table from './Table'
 import Form from './Form'
 
@@ -16,6 +17,25 @@ function MyApp()
     function updateList(person) {
         setCharacters([...characters, person]);
     }
+
+    async function fetchAll(){
+        try {
+           const response = await axios.get('http://localhost:5000/users');
+           return response.data.users_list;     
+        }
+        catch (error){
+           //We're not handling errors. Just logging into the console.
+           console.log(error); 
+           return false;         
+        }
+    }
+
+    useEffect(() => {
+        fetchAll().then( result => {
+           if (result)
+              setCharacters(result);
+         });
+     }, [] );
 
     return (
         <div className="container">
