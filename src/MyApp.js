e03b53f46dce9632d1b6e5b6297eef7179524d05
Vinay Rajaratnam <vjrajara@calpoly.edu>
2021-01-15 10:03:41 -0800

    Update ID on frontend with post call and include ID column

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,49 +1,67 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Table from './Table'
 import Form from './Form'
 
-function MyApp()
-{
+function MyApp() {
     const [characters, setCharacters] = useState([]);
 
     function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
             return i !== index
         });
-        setCharacters(updated);
+        makeDeleteCall(characters[index]).then(result => {
+            if (result)
+                setCharacters(updated);
+        })
     }
 
     function updateList(person) {
-        makePostCall(person).then( result => {
+        makePostCall(person).then(result => {
             if (result)
-                setCharacters([...characters, person] );
+                setCharacters([...characters, person]);
         });
     }
 
-    async function fetchAll(){
+    async function fetchAll() {
         try {
-           const response = await axios.get('http://localhost:5000/users');
-           return response.data.users_list;     
+            const response = await axios.get('http://localhost:5000/users');
+            return response.data.users_list;
         }
-        catch (error){
-           //We're not handling errors. Just logging into the console.
-           console.log(error); 
-           return false;         
+        catch (error) {
+            //We're not handling errors. Just logging into the console.
+            console.log(error);
+            return false;
         }
     }
 
     useEffect(() => {
-        fetchAll().then( result => {
-           if (result)
-              setCharacters(result);
-         });
-     }, [] );
+        fetchAll().then(result => {
+            if (result)
+                setCharacters(result);
+        });
+    }, []);
 
-    async function makePostCall(person){
+    async function makePostCall(person) {
         try {
             const response = await axios.post('http://localhost:5000/users', person);
-            return response;
+            person = response.data;
+            if (response.status == 201) {
+                return response;
+            }
+        }
+        catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async function makeDeleteCall(person) {
+        try {
+            const response = await axios.delete('http://localhost:5000/users/{person.id}');
+            if (response.status == 204) {
+                return response.data
+            }
         }
         catch (error) {
             console.log(error);
