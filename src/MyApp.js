e6c72f26df7d45355bc5a62e71e80c42d9712e66
Vinay Rajaratnam <vjrajara@calpoly.edu>
2021-01-14 16:42:30 -0800

    Made post call to update list

--- src/MyApp.js
+++ src/MyApp.js
@@ -15,7 +15,10 @@ function MyApp()
     }
 
     function updateList(person) {
-        setCharacters([...characters, person]);
+        makePostCall(person).then( result => {
+            if (result)
+                setCharacters([...characters, person] );
+        });
     }
 
     async function fetchAll(){
@@ -36,6 +39,17 @@ function MyApp()
               setCharacters(result);
          });
      }, [] );
+
+    async function makePostCall(person){
+        try {
+            const response = await axios.post('http://localhost:5000/users', person);
+            return response;
+        }
+        catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
 
     return (
         <div className="container">
